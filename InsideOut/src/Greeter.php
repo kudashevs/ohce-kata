e81bc3bf0d87dc1008ec44b6a1431b17313a3162
Sergey Kudashev <kudashevs@gmail.com>
2023-03-08 21:10:57 +0300

    Update Greeter add greeting during the day functionality

--- InsideOut/src/Greeter.php
+++ InsideOut/src/Greeter.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace OhceKata\InsideOut;
 
+use DateTime;
+
 class Greeter
 {
+    private DateTime $time;
+
+    public function __construct(DateTime $time)
+    {
+        $this->time = $time;
+    }
+
     public function process(string $name): string
     {
+        $hour = (int)$this->time->format('H');
+
+        if ($hour >= 6 && $hour <= 12) {
+            return "¡Buenos días {$name}!";
+        }
+
         return "¡Buenas noches {$name}!";
     }
 }
--- InsideOut/tests/GreeterTest.php
+++ InsideOut/tests/GreeterTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OhceKata\InsideOut\Tests;
 
+use DateTime;
 use OhceKata\InsideOut\Greeter;
 use PHPUnit\Framework\TestCase;
 
@@ -12,8 +13,22 @@ class GreeterTest extends TestCase
     /** @test */
     public function it_can_return_buenas_noches_pedro()
     {
-        $greeter = new Greeter();
+        $timeStub = $this->createStub(DateTime::class);
+        $timeStub->method('format')
+            ->willReturn('00');
+        $greeter = new Greeter($timeStub);
 
         $this->assertSame('¡Buenas noches Pedro!', $greeter->process('Pedro'));
+    }
+
+    /** @test */
+    public function it_can_return_buenos_dias_pedro()
+    {
+        $timeStub = $this->createStub(DateTime::class);
+        $timeStub->method('format')
+            ->willReturn('7');
+        $greeter = new Greeter($timeStub);
+
+        $this->assertSame('¡Buenos días Pedro!', $greeter->process('Pedro'));
     }
 }
