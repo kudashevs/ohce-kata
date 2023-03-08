caa2345473c33705b0f67ad67825d6500d3a1059
Sergey Kudashev <kudashevs@gmail.com>
2023-03-08 21:49:20 +0300

    Refactor GreeterTest extract data provider

--- InsideOut/tests/GreeterTest.php
+++ InsideOut/tests/GreeterTest.php
@@ -21,7 +21,7 @@ class GreeterTest extends TestCase
     }
 
     /** @test */
-    public function it_can_return_buenas_noches_pedro()
+    public function it_can_greet_pedro_in_the_midnight()
     {
         $timeStub = $this->createTimeStub('00:00');
         $greeter = new Greeter($timeStub);
@@ -29,22 +29,36 @@ class GreeterTest extends TestCase
         $this->assertSame('¡Buenas noches Pedro!', $greeter->greet('Pedro'));
     }
 
-    /** @test */
-    public function it_can_return_buenos_dias_pedro()
+    /**
+     * @test
+     * @dataProvider provideDifferentHoursForPedro
+     */
+    public function it_can_greet_pedro(DateTime $time, string $expected)
     {
-        $timeStub = $this->createTimeStub('7:00');
-        $greeter = new Greeter($timeStub);
+        $greeter = new Greeter($time);
 
-        $this->assertSame('¡Buenos días Pedro!', $greeter->greet('Pedro'));
+        $this->assertSame($expected, $greeter->greet('Pedro'));
     }
 
-    /** @test */
-    public function it_can_return_buenas_tardes_pedro()
+    public function provideDifferentHoursForPedro(): array
     {
-        $timeStub = $this->createTimeStub('13:00');
-        $greeter = new Greeter($timeStub);
-
-        $this->assertSame('¡Buenas tardes Pedro!', $greeter->greet('Pedro'));
+        /**
+         * 20:00 => 05:59 is equal to night
+         * 06:00 => 11:59 is equal to morning
+         * 12:00 => 19:59 is equal to noon
+         */
+        return [
+            'before night greeting' => [$this->createTimeStub('19:59'), '¡Buenas tardes Pedro!'],
+            'night greeting' => [$this->createTimeStub('20:00'), '¡Buenas noches Pedro!'],
+            'after night greeting' => [$this->createTimeStub('20:01'), '¡Buenas noches Pedro!'],
+            'midnight greeting' => [$this->createTimeStub('00:00'), '¡Buenas noches Pedro!'],
+            'before morning greeting' => [$this->createTimeStub('5:59'), '¡Buenas noches Pedro!'],
+            'morning greeting' => [$this->createTimeStub('6:00'), '¡Buenos días Pedro!'],
+            'after morning greeting' => [$this->createTimeStub('6:01'), '¡Buenos días Pedro!'],
+            'before noon greeting' => [$this->createTimeStub('11:59'), '¡Buenos días Pedro!'],
+            'noon greeting' => [$this->createTimeStub('12:00'), '¡Buenas tardes Pedro!'],
+            'after noon greeting' => [$this->createTimeStub('12:01'), '¡Buenas tardes Pedro!'],
+        ];
     }
 
     /**
