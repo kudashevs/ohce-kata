c71dfc4cf94028d5616d5e0f7ff496fac527e934
Sergey Kudashev <kudashevs@gmail.com>
2023-03-09 20:41:16 +0300

    Update Output add a CLI output implementation

---
+++ InsideOut/src/Output/CliOutput.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OhceKata\InsideOut\Output;
+
+class CliOutput implements OutputInterface
+{
+    public function writeLine(string $str): void
+    {
+        $trimmed = trim($str);
+
+        fwrite(STDOUT, $trimmed . PHP_EOL);
+    }
+
+    public function terminate(int $code = 0): void
+    {
+        exit($code);
+    }
+}
